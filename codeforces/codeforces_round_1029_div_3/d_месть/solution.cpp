50579a3afa17dc8a2267330335bf608f23852b9d
ILUNOSHA <dimakorolev2000@gmail.com>
2025-06-09 06:19:59 +0300

    cf 1029 div3 task d fix

--- codeforces/codeforces_round_1029_div_3/d_месть/solution.cpp
+++ codeforces/codeforces_round_1029_div_3/d_месть/solution.cpp
@@ -16,7 +16,7 @@ int main() {
         }
 
         bool is_arithmetic = true;
-        long long difference = arr.size() > 1 ? arr[1] - arr[0] : 0;
+        long long difference = arr[1] - arr[0];
 
         for (int i = 2; i < n; ++i) {
             if (arr[i] - arr[i - 1] != difference) {
